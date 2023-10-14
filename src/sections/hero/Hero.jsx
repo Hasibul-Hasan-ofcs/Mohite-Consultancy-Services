97343a9adefc8dc1b5767f4bc33862828a9379a8
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-10-14 10:22:08 +0600

    hero section updated, several fixes

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -6,18 +6,33 @@ import HeroImg3 from "../../assets/png/hero-img-3.png";
 const Hero = () => {
   return (
     <div className="container mx-auto flex items-center flex-col-reverse lg:flex-row">
-      <div className="w-full lg:w-1/2 text-center lg:text-start py-9 lg:py-0 min-h-fit lg:min-h-[calc(803px-85px)] flex flex-col justify-center">
+      <div className="w-full lg:w-1/2 text-center lg:text-start py-9 lg:py-0 min-h-fit lg:min-h-[calc(803px-85px)] flex flex-col justify-center items-center lg:items-start">
         <h1 className="text-[65px] leading-[80px] font-bold">
-          Find Partners (CAs) available online
+          Find{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#0076CE] to-[#9400D3]">
+            Partners
+          </span>{" "}
+          (CAs) available online
         </h1>
-        <small className="text-[#616161] text-xl">
+        <small className="text-[#616161] text-xl font-os pt-4 pb-[60px]">
           <span className="font-bold">CONNECT</span> with us where your services
           are listed and visible to a myriad of businesses seeking CA's for
           compliance support
         </small>
-        <form className="">
-          <input type="text" name="search_text" id="search_text" />
-          <button type="submit">Search</button>
+        <form className="bg-white h-[73px] flex w-[80%] rounded-[10px] overflow-hidden font-inter">
+          <input
+            type="text"
+            name="search_text"
+            id="search_text"
+            className="border-none outline-none bg-white h-full flex-grow w-[120px] md:w-auto px-4 lg:px-[30px] font-bold text-[18px]"
+            placeholder="Search by name"
+          />
+          <button
+            type="submit"
+            className="h-[73px] w-[186px] text-[16px] font-bold bg-[#0076ce] text-white border-[1.5px] border-[#0076ce] rounded-[10px] hover:border-white hover:text-[#0076ce] flex items-center justify-center transition_common hover:bg-white shadow-none hover:shadow-lg"
+          >
+            Search
+          </button>
         </form>
       </div>
       <div className="w-full lg:w-1/2 py-9 lg:py-0 min-h-fit lg:min-h-[calc(803px-85px)] flex flex-col justify-center items-center">
--- src/sections/navbar/NavBar.jsx
+++ src/sections/navbar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoMainBlack from "../../assets/png/logo-main-black.png";
 import { HiChevronDown } from "react-icons/hi";
+import { CgMenuRight } from "react-icons/cg";
 
 const linkData = [
   {
@@ -24,29 +25,32 @@ const linkData = [
 const NavBar = () => {
   return (
     <nav>
-      <div className="container mx-auto h-[85px] flex justify-between items-center">
+      <div className="container mx-auto h-[85px] flex justify-between items-center px-3 lg:px-0">
         <div className="flex items-center gap-[32px]">
           <img src={LogoMainBlack} alt="logo main black" />
-          <div className="flex gap-9">
-            {linkData.map(({ link, text }, indx) => (
-              <a
-                href={link}
-                className="flex items-center font-bold text-[18px] group"
-                key={indx}
-              >
-                <span>{text}</span>&nbsp;
-                <HiChevronDown className="text-[#0076ce] transition_common relative group-hover:translate-y-1" />
-              </a>
-            ))}
+          <div className="hidden lg:block">
+            <div className="flex gap-9">
+              {linkData.map(({ link, text }, indx) => (
+                <a
+                  href={link}
+                  className="flex items-center font-bold text-[18px] group"
+                  key={indx}
+                >
+                  <span>{text}</span>&nbsp;
+                  <HiChevronDown className="text-[#0076ce] transition_common relative group-hover:translate-y-1" />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
         <div className="flex items-center gap-4">
-          <button className="py-[10px] px-5 border-[1.5px] border-[#0076ce] text-[#0076ce] rounded-[10px] font-bold transition_common hover:bg-white shadow-none hover:shadow-lg hover:border-white">
+          <button className="hidden lg:block py-[10px] px-5 border-[1.5px] border-[#0076ce] text-[#0076ce] rounded-[10px] font-bold transition_common hover:bg-white shadow-none hover:shadow-lg hover:border-white">
             Login
           </button>
-          <button className="bg-[#0076ce] text-white border-[1.5px] border-[#0076ce] rounded-[10px] py-[10px] px-4 font-bold transition_common hover:bg-white shadow-none hover:shadow-lg hover:border-white hover:text-[#0076ce]">
+          <button className="hidden lg:block bg-[#0076ce] text-white border-[1.5px] border-[#0076ce] rounded-[10px] py-[10px] px-4 font-bold transition_common hover:bg-white shadow-none hover:shadow-lg hover:border-white hover:text-[#0076ce]">
             Register
           </button>
+          <CgMenuRight className="block lg:hidden text-2xl" />
         </div>
       </div>
     </nav>
